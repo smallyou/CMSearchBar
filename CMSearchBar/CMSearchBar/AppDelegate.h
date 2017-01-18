ca14b6d3ee1b48cd5f0abb1322614319ddcae3ef
smallyou <smallyou@126.com>
2017-01-18 11:12:37 +0800

    初始化项目

---
+++ CMSearchBar/CMSearchBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CMSearchBar
+//
+//  Created by 23 on 2017/1/18.
+//  Copyright © 2017年 23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CMSearchBar/CMSearchBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CMSearchBar
+//
+//  Created by 23 on 2017/1/18.
+//  Copyright © 2017年 23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
